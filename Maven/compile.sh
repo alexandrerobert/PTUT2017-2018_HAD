3317f0160449b690a7f08b801a0f428435fbe9b7
Alex <alexandre.tds.robert@gmail.com>
2018-06-14 23:39:47 +0200

    Docker Glassfish
    lack of glassfish user's settings for security.

--- Maven/compile.sh
+++ Maven/compile.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# Se place au niveau du projet
 cd ../MavenWebAppHADBPM/MavenWebAppHADBPM
 
+# Créé le .war
 mvn clean  install
+
+# Déplace le .war créé dans le dossier Docker
+cp $HOME/.m2/repository/com/mycompany/MavenWebAppHADBPM/1.0-SNAPSHOT/MavenWebAppHADBPM-1.0-SNAPSHOT.war ../../Docker/MavenWebHADBPMN-1.0-SNAPSHOT.war
